0b79082d06d68b0d1321b76178413a593e506a35
Val Neekman <val@neekware.com>
2012-09-16 21:27:24 -0700

    add automation for pypi

--- pushpi.sh
+++ pushpi.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+python setup.py register
+python setup.py bdist_egg upload --identity="val Neekman" --sign --quiet
+python setup.py bdist_wininst --target-version=2.7 register upload --identity="Val Neekman" --sign --quiet
+python setup.py sdist upload --identity="Val Neekman" --sign
 
